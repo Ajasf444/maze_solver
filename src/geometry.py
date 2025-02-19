57d170e3036f597dfb559907250a6191a7598eb3
Ajasf444 <longstephen444@gmail.com>
2025-02-19 17:40:10 -0600

    added Line class

--- src/geometry.py
+++ src/geometry.py
@@ -1,3 +1,6 @@
+from tkinter import Canvas
+
+
 class Point:
     def __init__(self, x, y):
         self.x = x
@@ -8,3 +11,8 @@ class Line:
     def __init__(self, start: Point, end: Point):
         self.start = start
         self.end = end
+
+    def draw(self, canvas: Canvas, fill_color):
+        x1, y1 = self.start.x, self.start.y
+        x2, y2 = self.end.x, self.end.y
+        canvas.create_line(x1, y1, x2, y2, fill=fill_color, width=2)
