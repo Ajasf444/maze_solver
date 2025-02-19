00d90aeb0fb78973a29364b2709640b340c2e248
Ajasf444 <longstephen444@gmail.com>
2025-02-19 00:12:45 -0600

    finished up Window class __init__()

--- src/window.py
+++ src/window.py
@@ -5,5 +5,7 @@ class Window:
     DEFAULT_TITLE = "Baconator Fries"
 
     def __init__(self, width, height):
-        self.root = Tk(DEFAULT_TITLE)
+        self.root = Tk(Window.DEFAULT_TITLE)
         self.canvas = Canvas()
+        self.canvas.pack()
+        self.is_running = False
