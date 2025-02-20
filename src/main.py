69fb69be002416bb67814c97d767559fe974f65b
Ajasf444 <longstephen444@gmail.com>
2025-02-20 12:02:22 -0600

    added line to test drawing

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 from window import Window
+from geometry import Point, Line
 
 
 def main():
     win = Window(800, 600)
+    pA, pB = Point(0, 0), Point(400, 300)
+    line = Line(pA, pB)
+    win.draw_line(line, "black")
+
     win.wait_for_close()
 
 
