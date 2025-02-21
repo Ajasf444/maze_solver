9f2f692d3ae292d626b3a6b8a3e5eb4a0ed9c7a0
Ajasf444 <longstephen444@gmail.com>
2025-02-21 16:51:48 -0600

    added another Cell to test

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
+from cell import Cell
+from geometry import Line, Point
 from window import Window
-from geometry import Point, Line
 
 
 def main():
@@ -10,6 +11,12 @@ def main():
     pC, pD = Point(400, 0), Point(0, 300)
     line = Line(pC, pD)
     win.draw_line(line, "red")
+    cellA = Cell(pA, pB, win)
+    cellB = Cell(
+        Point(400, 400), Point(500, 500), win, has_left_wall=False, has_right_wall=False
+    )
+    cellA.draw()
+    cellB.draw()
 
     win.wait_for_close()
 
