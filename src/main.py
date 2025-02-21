63b9f89cb37744f44f5bd40ee0853a3d1fd5bf8a
Ajasf444 <longstephen444@gmail.com>
2025-02-20 21:08:23 -0600

    added a second red line

--- src/main.py
+++ src/main.py
@@ -7,6 +7,9 @@ def main():
     pA, pB = Point(0, 0), Point(400, 300)
     line = Line(pA, pB)
     win.draw_line(line, "black")
+    pC, pD = Point(400, 0), Point(0, 300)
+    line = Line(pC, pD)
+    win.draw_line(line, "red")
 
     win.wait_for_close()
 
