327e6cf1a59603e3d959e9aaf8ea2fecaa9bab8c
Ajasf444 <longstephen444@gmail.com>
2025-02-25 01:55:57 -0600

    added unequal sized cells for a maze test

--- src/test_maze.py
+++ src/test_maze.py
@@ -11,6 +11,13 @@ class tests(unittest.TestCase):
         self.assertEqual(len(m1._cells), num_cols)
         self.assertEqual(len(m1._cells[0]), num_rows)
 
+    def test_maze_create_unequal_size_cells(self):
+        num_cols = 10
+        num_rows = 10
+        m1 = Maze(Point(0, 0), num_rows, num_cols, 20, 10)
+        self.assertEqual(len(m1._cells), num_cols)
+        self.assertEqual(len(m1._cells[0]), num_rows)
+
 
 if __name__ == "__main__":
     unittest.main()
