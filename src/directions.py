f549fa6b8f854ac099211b874d9b512743e0efaa
Ajasf444 <longstephen444@gmail.com>
2025-03-09 22:37:47 -0500

    added Directions enum for managing cell directions

---
+++ src/directions.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class Directions(Enum):
+    RIGHT = None
+    UP = None
+    LEFT = None
+    DOWN = None
+
+    def __init__(self, i, j):
+        self.RIGHT = (i, j + 1)
+        self.UP = (i - 1, j)
+        self.LEFT = (i, j - 1)
+        self.DOWN = (i + 1, j)
