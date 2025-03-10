79ba282c0017fe9ab886fa42e9cb75bb6592a050
Ajasf444 <longstephen444@gmail.com>
2025-03-10 10:18:59 -0500

    added test for _reset_cells_visited()

--- src/test_maze.py
+++ src/test_maze.py
@@ -26,6 +26,17 @@ class tests(unittest.TestCase):
         self.assertEqual(m1._cells[0][0].has_top_wall, False)
         self.assertEqual(m1._cells[-1][-1].has_bottom_wall, False)
 
+    def test_maze_reset_visited(self):
+        num_cols = 10
+        num_rows = 10
+        m1 = Maze(Point(0, 0), num_rows, num_cols, 20, 10)
+        m1._break_entrance_and_exit()
+        m1._break_wall_r(0, 0)
+        m1._reset_cells_visited()
+        for column in m1._cells:
+            for cell in column:
+                self.assertEqual(cell.visited, False)
+
 
 if __name__ == "__main__":
     unittest.main()
