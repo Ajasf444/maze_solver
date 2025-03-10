ab1973df6461e7c0251506949c3d554ce7dd4b18
Ajasf444 <longstephen444@gmail.com>
2025-03-10 02:56:35 -0500

    tested _break_wall_r() and added comments for recursion limit changing

--- src/main.py
+++ src/main.py
@@ -2,26 +2,29 @@ from cell import Cell
 from maze import Maze
 from geometry import Line, Point
 from window import Window
+# import sys
 
 
 def main():
+    # sys.setrecursionlimit(20_000)
     win = Window(800, 600)
-    pA, pB = Point(0, 0), Point(400, 300)
-    line = Line(pA, pB)
-    win.draw_line(line, "black")
-    pC, pD = Point(400, 0), Point(0, 300)
-    line = Line(pC, pD)
-    win.draw_line(line, "red")
-    cellA = Cell(pA, pB, win)
-    cellB = Cell(
-        Point(400, 400), Point(500, 500), win, has_left_wall=False, has_right_wall=False
-    )
-    cellA.draw()
-    cellB.draw()
-    cellA.draw_move(cellB)
+    pA, pB = Point(20, 20), Point(400, 300)
+    #    line = Line(pA, pB)
+    #    win.draw_line(line, "black")
+    #    pC, pD = Point(400, 0), Point(0, 300)
+    #    line = Line(pC, pD)
+    #    win.draw_line(line, "red")
+    #    cellA = Cell(pA, pB, win)
+    #    cellB = Cell(
+    #        Point(400, 400), Point(500, 500), win, has_left_wall=False, has_right_wall=False
+    #    )
+    #    cellA.draw()
+    #    cellB.draw()
+    #    cellA.draw_move(cellB)
 
-    maze = Maze(pA, 5, 5, 50, 50, win)
+    maze = Maze(pA, 10, 10, 50, 50, win)
     maze._break_entrance_and_exit()
+    maze._break_wall_r(0, 0)
     win.wait_for_close()
 
 
